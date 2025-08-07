04949d8d0b03843feef83b387151a3718dfa200a
Mr.None <thewitcher745@gmail.com>
2025-08-07 16:29:38 +0330

    FEAT: Added top_gainers caching
    - Now caches top_gainers results for 3 hours

--- app_prepare.py
+++ app_prepare.py
@@ -1,6 +1,11 @@
 from flask_cors import CORS
 from flask import Flask
+from flask_caching import Cache
 
 app = Flask(__name__)
 
 CORS(app)  # This enables CORS for all routes
+
+# Enabling caching on the app
+cache = Cache(config={"CACHE_TYPE": "SimpleCache", "CACHE_DEFAULT_TIMEOUT": 3600})
+cache.init_app(app)
--- routes/technicals/top_gainers/endpoint.py
+++ routes/technicals/top_gainers/endpoint.py
@@ -1,9 +1,21 @@
+"""
+This module will contain the endpoint handlers for the top_gainers table.
+"""
+
 from flask import jsonify
 
-from app_prepare import app
+from app_prepare import app, cache
 from .data_fetching import TopGainers
 
 
 @app.route("/api/top_gainers")
 def get_top_gainers():
-    return jsonify(TopGainers.scrape_data())
+    data = cache.get("top_gainers")
+
+    # If no cache is found, fetch data again, and set the cache.
+    if not data:
+        print("Scraping data")
+        data = TopGainers.scrape_data()
+        cache.set("top_gainers", data, timeout=10800)
+
+    return jsonify(data)
