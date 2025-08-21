45d8984de040aa95939704e6a2cfaa494d522015
Mr.None <thewitcher745@gmail.com>
2025-08-21 18:15:18 +0330

    FIX: Minor bugfix

--- routes/technicals/urls.py
+++ routes/technicals/urls.py
@@ -4,5 +4,5 @@ This file contains the URL's and endpoints for fetching or scraping data.
 
 TOP_GAINERS_LOSERS = "https://coinmarketcap.com/gainers-losers/"
 TRENDING = "https://coinmarketcap.com/trending-cryptocurrencies/"
-HEATMAP = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&price_change_percentage=24h"
+HEATMAP = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD"
 RECENT_AIRDROPS = "https://airdrops.io/latest/"
