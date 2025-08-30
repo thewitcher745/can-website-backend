192c25b496b3d79becae0530a979a9c84c50fb4a
Mr.None <thewitcher745@gmail.com>
2025-08-30 18:21:30 +0330

    FIX: Fixed nonexistent import

--- routes/__init__.py
+++ routes/__init__.py
@@ -9,6 +9,6 @@ import routes.technicals.top_gainers
 import routes.technicals.top_losers
 import routes.technicals.trending
 import routes.technicals.recent_airdrops
-import routes.technicals.coins_tables_summary
+# import routes.technicals.coins_tables_summary
 import routes.logos.get_logo_link
 import routes.technicals.crypto_heatmap
