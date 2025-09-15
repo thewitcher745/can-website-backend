e6ded783d39a34f0dc3f54c543d18d64b41f6bb0
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-09-15 11:21:17 +0330

    REF: Changed /api/market_data to use JS naming conventions in response

--- routes/technicals/market_data/data_fetching.py
+++ routes/technicals/market_data/data_fetching.py
@@ -1,6 +1,7 @@
 import requests
 from ..urls import MARKET_DATA
 
+
 def fetch_market_data():
     """
     Fetch BTC and ETH market cap dominance and total market cap change percentage from CoinGecko MARKET_DATA endpoint.
@@ -14,15 +15,17 @@ def fetch_market_data():
     """
     response = requests.get(MARKET_DATA)
     response.raise_for_status()
-    data = response.json().get('data', {})
+    data = response.json().get("data", {})
 
-    market_cap_percentage = data.get('market_cap_percentage', {})
-    btc_dominance = market_cap_percentage.get('btc')
-    eth_dominance = market_cap_percentage.get('eth')
-    market_cap_change_percentage_24h_usd = data.get('market_cap_change_percentage_24h_usd')
+    market_cap_percentage = data.get("market_cap_percentage", {})
+    btc_dominance = market_cap_percentage.get("btc")
+    eth_dominance = market_cap_percentage.get("eth")
+    market_cap_change_percentage_24h_usd = data.get(
+        "market_cap_change_percentage_24h_usd"
+    )
 
     return {
-        'btc_dominance': btc_dominance,
-        'eth_dominance': eth_dominance,
-        'market_cap_change_percentage_24h_usd': market_cap_change_percentage_24h_usd
+        "btcDominance": btc_dominance,
+        "ethDominance": eth_dominance,
+        "marketCapChangePercentage24h": market_cap_change_percentage_24h_usd,
     }
