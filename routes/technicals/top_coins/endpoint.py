9088d5da757b373d5e98e2b7685ab162aff9ed5b
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-09-23 16:54:01 +0330

    FEAT: Separated top_coins endppoints into by volume and by market cap

--- routes/technicals/top_coins/endpoint.py
+++ routes/technicals/top_coins/endpoint.py
@@ -8,7 +8,7 @@ from app_prepare import app, cache
 from .data_fetching import TopCoins
 
 
-@app.route("/api/top_coins")
+@app.route("/api/top_market_cap_coins")
 def get_top_coins():
     """
     Endpoint to get the list of top cryptocurrencies.
@@ -17,11 +17,30 @@ def get_top_coins():
     Returns:
         JSON response containing the list of top coins.
     """
-    data = cache.get("top_coins")
+    data = cache.get("top_market_cap_coins")
 
     # If no cache is found, fetch data and set the cache.
     if not data:
         data = TopCoins.fetch_data()
-        cache.set("top_coins", data, timeout=3600)
+        cache.set("top_market_cap_coins", data, timeout=3600)
+
+    return jsonify(data)
+
+
+@app.route("/api/top_volume_coins")
+def get_top_volume_coins():
+    """
+    Endpoint to get the list of top cryptocurrencies (by 24h volume).
+    Uses caching to avoid frequent scraping.
+
+    Returns:
+        JSON response containing the list of top coins.
+    """
+    data = cache.get("top_volume_coins")
+
+    # If no cache is found, fetch data and set the cache.
+    if not data:
+        data = TopCoins.fetch_data(sort_by="volume_24h")
+        cache.set("top_volume_coins", data, timeout=3600)
 
     return jsonify(data)
