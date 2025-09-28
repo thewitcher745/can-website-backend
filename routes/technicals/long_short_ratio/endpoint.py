f798f390a96ba42b20a419fb887ba016dcd8cbc3
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2025-09-28 11:18:21 +0330

    FIX: Fixed long_short_ratio caching

--- routes/technicals/long_short_ratio/endpoint.py
+++ routes/technicals/long_short_ratio/endpoint.py
@@ -18,14 +18,14 @@ def get_long_short_ratio():
         JSON response containing the long/short ratio data.
     """
     symbol = request.args.get("symbol")
-
-    data = cache.get("long_short_ratio")
-
-    # If no cache is found, fetch data and set the cache.
     if not symbol:
         return jsonify({"error": "Symbol not provided"}), 400
 
-    data = LongShortRatio(symbol).fetch_data()
-    cache.set("long_short_ratio", data, timeout=3600)
+    data = cache.get("long_short_ratio_" + symbol.lower())
+
+    # If no cache is found, fetch data and set the cache.
+    if not data:
+        data = LongShortRatio(symbol).fetch_data()
+        cache.set("long_short_ratio_" + symbol.lower(), data, timeout=3600)
 
     return jsonify(data)
